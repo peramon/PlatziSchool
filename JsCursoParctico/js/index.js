66fd18f51806a581f0c14e6b87137443beaf11c8
Paul Ramon <peramon98.18@gmail.com>
2022-04-21 15:09:36 -0500

    Transformando en funciones el codigo para calcular el area y perimetro de las figuras geometricas

--- JsCursoParctico/js/index.js
+++ JsCursoParctico/js/index.js
@@ -1,7 +1,7 @@
 // Cuadrado
 console.group('Cuadrados');
 
-const ladoCuadrado = 5;
+/*const ladoCuadrado = 5;
 
 console.log('Los lados del cuadrado miden: ' + ladoCuadrado + ' cm')
 
@@ -10,11 +10,24 @@ console.log('El perimetro es: ' + perimetroCuadrado + ' cm');
 
 const areaCuadrado = ladoCuadrado * ladoCuadrado;
 console.log('El area del cuadrado es: ' + areaCuadrado + ' cm^2');
+*/
+function calcularPerimetroCuadrado(lado){
+    return lado * 4;
+}
 
+function calcularAreaCuadrado(lado){
+    return lado * lado;
+}
+
+console.log(calcularPerimetroCuadrado(12));
+console.log(calcularAreaCuadrado(12));
 console.groupEnd();
+
+
 
 // Triangulo
 console.group('Triangulos');
+/*
 const ladoTrianguloUno = 6;
 const ladoTrianguloDos = 6;
 const baseTriangulo = 6;
@@ -29,12 +42,23 @@ const perimetroTriangulo = ladoTrianguloUno + ladoTrianguloDos + baseTriangulo;
 console.log('El perimetro es: ' + perimetroTriangulo + ' cm');
 
 const areaTriangulo = (baseTriangulo * alturaTriangulo)/2;
-console.log('El area del es: ' + areaTriangulo + ' cm^2')
+console.log('El area del es: ' + areaTriangulo + ' cm^2')*/
+function calcularPerimetroTriangulo(ladoUno,ladoDos, base){
+    return ladoUno + ladoDos + base;
+}
+
+function calcularAreaTriangulo(base, altura){
+    return (base * altura)/2;
+}
+
+console.log(calcularPerimetroTriangulo(2,2,4));
+console.log(calcularAreaTriangulo(2,4));
+
 console.groupEnd();
 // Circulo
 
 console.group('Circulos');
-const diametroCirculo = 8;
+/*const diametroCirculo = 8;
 const radioCirculo = diametroCirculo/2;
 
 console.log('Medidas del circulo -> ' 
@@ -45,5 +69,22 @@ const perimetroCirculo =  Math.PI * diametroCirculo;
 console.log('El perimetro es: ' + perimetroCirculo + ' cm');
 
 const areaCirculo = Math.PI * (radioCirculo * radioCirculo );
-console.log('El area es: ' + areaCirculo + ' cm^2')
+console.log('El area es: ' + areaCirculo + ' cm^2')*/
+
+function calcularPerimetroCirculo(diametro){
+    return Math.PI * diametro;
+}
+
+function calcularAreaCirculo(diametro){
+    let radio = calcularRadio(diametro);
+    return Math.PI * (radio * radio);
+}
+
+function calcularRadio(diametro){
+    return diametro/2;
+}
+
+console.log(calcularPerimetroCirculo(8));
+console.log(calcularAreaCirculo(8));
+
 console.groupEnd();
